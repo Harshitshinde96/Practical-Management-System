57827136f5b65a8cbeb91a0b4a8b0722ca891a38
=Harshitshinde96 <=harshitshinde65@gmail.com>
2024-11-17 23:19:03 +0530

    gitignore added

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import express from "express";
 const app = express();
 const PORT = 3000;
 // const PORT = 3000 || process.env.PORT;
-
+let a=1;
 app.listen(PORT, () => {
   console.log(`Serving on port ${PORT}`);
 });
