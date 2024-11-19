7cc27665937773f479489ae319cf9bdb45e1a1c4
=Harshitshinde96 <=harshitshinde65@gmail.com>
2024-11-19 12:02:22 +0530

    project update

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 import express from "express";
+import dotenv from "dotenv"
+import mongoose from "mongoose";
+import dbConnect from "./config/database";
+
+
+
+
+
 
 const app = express();
 const PORT = 3000;
 // const PORT = 3000 || process.env.PORT;
+
+
+
+
+
+
+
+
+
 
 app.listen(PORT, () => {
   console.log(`Serving on port ${PORT}`);
