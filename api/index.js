3e99b25dc3c418f288e5761bdb2896a5e6b4b34f
=Harshitshinde96 <=harshitshinde65@gmail.com>
2024-11-23 19:13:00 +0530

    Commit Changes

--- api/index.js
+++ api/index.js
@@ -1,17 +1,24 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
-import dbConnect from "./config/database.js";
-import router from "./routes/userRoutes.js";
+import dbConnect from "../config/database.js";
+import router from "../routes/userRoutes.js";
 
 const app = express();
 
 dotenv.config();
-const PORT = 3000;
-// const PORT = 3000 || process.env.PORT;
+// const PORT = 3000;
+const PORT = 3000 || process.env.PORT;
 app.use(express.json());
 
 app.use("/api/v1", router);
+
+app.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "Server Running Successfully",
+  });
+});
 
 dbConnect();
 
